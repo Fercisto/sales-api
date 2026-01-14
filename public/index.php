f8d780c6fd749690f01e38cef7ff72acbac4663c
Fercisto <dedtroll314@hotmail.com>
2026-01-13 18:44:32 -0600

    Dando permisos de cabecera

--- public/index.php
+++ public/index.php
@@ -1,7 +1,8 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
